09e9b7856c5c55d9b9f875d47a6c3f20d6f1883d
rasifashrafi <rashrafi@techlaunch.io>
2018-08-13 18:43:17 -0400

    first commit

---
+++ Q10.js
@@ -0,0 +1,14 @@
+// Create a new function called "efficient" below the "msytery" function that returns
+// the same value as the mystery function. However, the efficient function
+// should have only one line of code inside its body instead of 4 lines of code,
+// like the mystery function has.
+
+function mystery(x, y) {
+  var a = 4 * x * y;
+  var b = 3 * y + 5;
+  var c = a + b;
+  return c;
+}
+// console.log(mystery(1,2))
+function efficient (x,y){ return c = (4 * x * y) + (3 * y + 5);}
+// console.log(efficient(1,2))
